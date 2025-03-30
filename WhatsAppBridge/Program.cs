1e5d440727a960e08d0efc8ca86e5b2173e31eae
Nuri YILMAZ <dengere@gmail.com>
2025-03-30 12:41:45 +0300

    Add project files.

---
+++ WhatsAppBridge/Program.cs
@@ -0,0 +1,17 @@
+internal class Program {
+    private static void Main() {
+        var builder = WebApplication.CreateBuilder();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
